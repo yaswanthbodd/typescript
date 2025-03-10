b557260ed48a5d584a94970502207acb81c4a314
yaswanthbodd <bodduyaswanth09@gmail.com>
2025-03-10 23:29:52 +0530

    typescript

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,6 @@
+// Data types
 let myName:string = 'yaswanth'
-myName = 56
+let year: number = 19;
+let isLoading : boolean = true;
+let album : any = 1999; // any type
+let album1 : string | number | boolean // union type
